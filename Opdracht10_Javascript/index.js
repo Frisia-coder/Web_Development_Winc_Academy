2a71e26a1d8f2a0c355b38df65360282e8a32ccd
Frisia-coder <93887912+Frisia-coder@users.noreply.github.com>
2022-01-09 20:35:50 +0100

    Add files via upload

---
+++ Opdracht10_Javascript/index.js
@@ -0,0 +1,35 @@
+console.log('Hello Winc Academy');
+let name='Michael';
+console.log(name);
+    //if (condition) 
+    //  block of code to be executed if the condition is true
+    // kan niet uitgevoerd worden als variabel.
+let addition='4'+'4';
+console.log(addition);
+   // 2 cijvers optellen als 4+4 is niet mogelijk als een som. Dit blijft dan 4+4.
+   // als een string lukt het wel, maar dan wordt de uitslag 44. Dus dan worden de cijvers naast elkaar gezet.
+   // Vermenigvuldigen, delen en aftrekken lukt wel als een string.
+let substraction='4'-'4';
+console.log(substraction);
+let multiplication='2'*'7';
+console.log(multiplication);
+let division='9'/'3';
+console.log(division);
+let Y=5;
+let X=Y % 2;
+console.log(X);
+   // Modulus kan op deze manier
+let age=38;
+   // Age in numbers for typeof
+let age_=('38');
+   // Age as an string for typeof
+let y=5;
+let x=y+2;
+console.log(x)
+   // Op deze manier kun je ook optellen.
+
+
+
+
+
+  
